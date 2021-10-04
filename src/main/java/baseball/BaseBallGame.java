15661ff1ceef4dc487e4855733f7977abbdab8af
sanghyuki83 <sanghyuki83@gmail.com>
2021-10-04 21:56:28 +0900

    feat: gameStart, setNumber
    set the goal numbers after game-start

---
+++ src/main/java/baseball/BaseBallGame.java
@@ -0,0 +1,61 @@
+package baseball;
+
+import nextstep.utils.Randoms;
+
+public class BaseBallGame {
+    private static final BaseBallGame game = new BaseBallGame();
+
+    private boolean continuance;
+    private Numbers goal;
+
+    private BaseBallGame() {}
+
+    public static BaseBallGame getInstance(){
+        return game;
+    }
+
+    public void gameStart(){
+        continuance = true;
+
+        while (continuance){
+            setNumber();
+            playBall();
+            askPlay();
+        }
+    }
+
+    private void setNumber() {
+        int first = makeNumber();
+        int second = makeNumber(first);
+        int third = makeNumber(first, second);
+
+        goal = new Numbers(first, second, third);
+    }
+
+    private int makeNumber(int number1, int number2) {
+        int value = number1;
+        while (value == number1 || value == number2){
+            value = Randoms.pickNumberInRange(0, 9);
+        }
+        return value;
+    }
+
+    private int makeNumber(int number) {
+        int value = number;
+        while (value == number){
+            value = Randoms.pickNumberInRange(0, 9);
+        }
+        return value;
+    }
+
+    private int makeNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+    private void askPlay() {
+        continuance = false;
+    }
+
+    private void playBall() {
+    }
+}
---
+++ src/main/java/baseball/Numbers.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class Numbers {
+    public int firstNumber;
+    public int secondNumber;
+    public int thirdNumber;
+
+    public Numbers(int firstNumber, int secondNumber, int thirdNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+        this.thirdNumber = thirdNumber;
+    }
+}
